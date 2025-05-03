1bdda7cfe3c0eaa234bbe0957b18f7169242e018
Brunin12 <brunoparreira77@gmail.com>
2025-05-03 09:58:21 -0300

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/app/components/Navbar";
+import { SessionProvider } from "next-auth/react";
+
+export default function Layout({children}: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <main className="font-work-sans">
+            <SessionProvider>
+                <Navbar />
+                {children}
+            </SessionProvider>
+        </main>
+    )
+}
